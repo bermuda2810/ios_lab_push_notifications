50be1d7f7d56c537a72341f297637cf8b977ea1a
VietBQ <bqviet.vn@gmail.com>
2016-08-05 11:21:15 +0700

    Initial Commit

---
+++ XShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XShop
+//
+//  Created by Bui Quoc Viet on 8/5/16.
+//  Copyright © 2016 NAL Viet Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XShop
+//
+//  Created by Bui Quoc Viet on 8/5/16.
+//  Copyright © 2016 NAL Viet Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
